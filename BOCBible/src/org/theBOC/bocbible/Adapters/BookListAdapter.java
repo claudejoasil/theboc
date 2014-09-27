6f04c5470ee4f83e00c3aa8c31ceac5ac257434f
cjoasil@gmail.com <cjoasil@gmail.com@4229e89a-cad6-7d9b-ad1d-ea27c5726503>
2014-09-27 14:25:09 +0000

    Update

--- BOCBible/src/org/theBOC/bocbible/Adapters/BookListAdapter.java
+++ BOCBible/src/org/theBOC/bocbible/Adapters/BookListAdapter.java
@@ -5,16 +5,22 @@ import org.theBOC.bocbible.R;
 import org.theBOC.bocbible.Models.Book;
 import org.theBOC.bocbible.common.BibleHelper;
 import org.theBOC.bocbible.common.MiscHelper;
+import org.theBOC.bocbible.database.Bible;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.GridView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 public class BookListAdapter extends BaseExpandableListAdapter {
@@ -22,6 +28,7 @@ public class BookListAdapter extends BaseExpandableListAdapter {
 	private ArrayList<Book> books;
 	private BibleHelper bibleHelper;
 	private MiscHelper miscHelper;
+	private Bible bibleDB;
 	public static final String BookId = "bookIdKey";
 	public static final String Chapter = "chapterKey";
 	public static final String Language = "languageKey";
@@ -88,14 +95,11 @@ public class BookListAdapter extends BaseExpandableListAdapter {
 	@Override
 	public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
 		final Book book = (Book) getGroup(groupPosition);
-		final Context theContext = this.context;
 		int numChapters = book.getNumChapters();
 		GridView chapterGridView;
 		 final int SPACING = (int) parent.getContext().getResources().getDimension(R.dimen.boc_default_gridItem_spacing);
 		 final int COL_WIDTH = (int) parent.getContext().getResources().getDimension(R.dimen.chapter_column_width);
 		 final int ROW_HEIGHT = (int) parent.getContext().getResources().getDimension(R.dimen.boc_default_gridItem_height);
-		 //final int PADDING = (int) parent.getContext().getResources().getDimension(R.dimen.activity_horizontal_margin);
-		 
 		 LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		 convertView = (ViewGroup) inflater.inflate(R.layout.activity_chapters, parent, false);
 		 chapterGridView = (GridView) convertView.findViewById(R.id.gridview);
@@ -132,13 +136,7 @@ public class BookListAdapter extends BaseExpandableListAdapter {
 		 {
 	          public void onItemClick(AdapterView<?> parent, View view, int position, long id) 
 	          {
-	        	  bibleHelper.setCurrentBookId(book.getBookId());
-	              bibleHelper.setCurrentChapter(position + 1);
-	              bibleHelper.setCurrentVerse(1);
-	              bibleHelper.setCurrentTestament(book.getTestament());
-	              miscHelper = MiscHelper.getInstance(theContext);
-	              miscHelper.reloadBiblePage = true;
-	        	  ((Activity)theContext).finish();
+	        	 showPopup(view, book, position);
 	          } 
 		 });
 		 chapterGridView.getLayoutParams().height = gridHeight; 
@@ -151,33 +149,51 @@ public class BookListAdapter extends BaseExpandableListAdapter {
 		// TODO Auto-generated method stub
 		return true;
 	}
+	
+	public void showPopup(View anchorView, final Book book, int position) {
 
-	/*
-	@Override
-	public int getCount() {
-		return books.size();
+	    View popupView = ((Activity) context).getLayoutInflater().inflate(R.layout.activity_verse_grid, null, false);
+
+	    final PopupWindow popupWindow = new PopupWindow(popupView, 
+	                           LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+
+	    TextView txtText = (TextView) popupView.findViewById(R.id.txtVerseInt);
+    	String tvString = book.getName() + " " + (position + 1) + ":" + "?";
+    	txtText.setText(tvString);
+	    txtText.setTypeface(null, Typeface.BOLD);
+    	txtText.setTextColor(0xFF813f15);
+    	final int SPACING = (int) ((Activity) context).getResources().getDimension(R.dimen.boc_default_gridItem_spacing);
+    	GridView verseGridView;
+    	verseGridView = (GridView) popupView.findViewById(R.id.verseGridview);
+    	verseGridView.setVerticalSpacing(SPACING);
+    	verseGridView.setHorizontalSpacing(SPACING);
+		 bibleHelper = BibleHelper.getInstance(this.context);
+		 final int selectedChapter = position + 1;
+		 bibleDB = new Bible(this.context);
+		 int numVerses = bibleDB.GetNumVerses(book.getBookId(), selectedChapter, bibleHelper.getCurrentVersionName("KJV"));
+		 verseGridView.setAdapter(new verseGridAdapter(context, numVerses));
+		 verseGridView.setOnItemClickListener(new OnItemClickListener() 
+		 {
+	          public void onItemClick(AdapterView<?> parent, View view, int position, long id) 
+	          {
+	        	  bibleHelper.setCurrentBookId(book.getBookId());
+	              bibleHelper.setCurrentChapter(selectedChapter);
+	              bibleHelper.setCurrentVerse(position + 1);
+	              bibleHelper.setCurrentTestament(book.getTestament());
+	              miscHelper = MiscHelper.getInstance(context);
+	              miscHelper.reloadBiblePage = true;
+	              popupWindow.dismiss();
+	        	  ((Activity) context).finish();
+	          } 
+		 });
+
+	    // If the PopupWindow should be focusable
+	    popupWindow.setFocusable(true);
+
+	    // If you need the PopupWindow to dismiss when when touched outside 
+	    popupWindow.setBackgroundDrawable(new ColorDrawable());
+	    popupWindow.showAtLocation(anchorView, Gravity.CENTER_VERTICAL, 0, 0);
 	}
 
-	@Override
-	public Object getItem(int position) {		
-		return books.get(position);
-	}
-
-	@Override
-	public long getItemId(int position) {
-		return position;
-	}
-
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);        
-        Book book = books.get(position);
-		if(convertView == null)
-			convertView = mInflater.inflate(R.layout.book_item, parent, false);
-		TextView txtNameText = (TextView) convertView.findViewById(R.id.book_text); 
-        txtNameText.setText(book.getName());  
-        return convertView;
-	}
-	*/
 
 }
